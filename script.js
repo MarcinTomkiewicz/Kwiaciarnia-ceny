83724dd1fa8d0b8e9ca593ccf4c30d1d93e31f56
Marcin Tomkiewicz <marc.tomk@gmail.com>
2021-03-25 11:29:57 +0100

    Pictures after click fade in

--- script.js
+++ script.js
@@ -2,17 +2,30 @@ const pictureSelect = document.querySelectorAll(".presentation");
 const largeDiv = document.querySelector('.large__foto');
 const largeImg = document.querySelector('.large__foto--element');
 const closeLargeDiv = document.querySelector('.close');
+let fps = 30;
 
 for (let pictureSelected of pictureSelect) {
     pictureSelected.addEventListener('click', () => {
         let pictureSRC = pictureSelected.src;
-        largeDiv.style.display = "block";
+        largeDiv.classList.add('large__foto--display');
         largeImg.src = pictureSRC;
-        console.log(pictureSelected);
-
     });
 }
 
-closeLargeDiv.addEventListener('click', () => {
-    largeDiv.style.display = "none";
-})
+largeDiv.addEventListener('click', (e) => {
+    if (e.target != largeImg)
+        largeDiv.classList.remove('large__foto--display');
+});
+
+// let start = null;
+// const animation = () => {
+//     setTimeout(function() {
+//         for (let i = 0; i <= 500; i++) {
+//             largeImg.style.width = i + "px";
+//         }
+//         requestAnimationFrame(animation);
+//     }, 1000 / fps);
+// }
+// animation();
+// console.log(pictureSelected);
+// });
