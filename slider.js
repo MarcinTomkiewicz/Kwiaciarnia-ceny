d6eb93cbb53351d7643808b13b1219af44b9e3be
Marcin Tomkiewicz <marc.tomk@gmail.com>
2021-04-09 10:07:08 +0200

    All HTML files added, adjusted features

--- slider.js
+++ slider.js
@@ -6,7 +6,7 @@ const sliderNextImage = document.createElement('img');
 const btnSlideLeft = document.querySelector('.left__arrow');
 const btnSlideRight = document.querySelector('.right__arrow');
 
-const flowerImages = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png'];
+const flowerImages = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png', '12.png'];
 
 let i = 0;
 
